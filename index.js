30c9f4d7d0b2f4e9071eab1c62ec6945a7244abb
ilovetyfng <enderkiller3472@gmail.com>
2022-01-29 20:00:48 +0800

    Add game function

--- index.js
+++ index.js
@@ -11,3 +11,68 @@ function computerPlay () {
             return "wtf";
     }
 }
+
+function rockPaperScissors (playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    let temp = -2;
+    switch (computerSelection) {
+        case "Rock":
+            switch (playerSelection){
+                case "Rock":
+                    temp = 0;
+                    break;
+                case "Paper":
+                    temp = 1;
+                    break;
+                case "Scissor":
+                    temp = -1;
+                    break;
+            }
+            break;
+        case "Paper":
+            switch (playerSelection){
+                case "Rock":
+                    temp = -1;
+                    break;
+                case "Paper":
+                    temp = 0;
+                    break;
+                case "Scissor":
+                    temp = 1;
+                    break;
+            }
+            break;
+        case "Scissor":
+            switch (playerSelection){
+                case "Rock":
+                    temp = 1;
+                    break;
+                case "Paper":
+                    temp = -1;
+                    break;
+                case "Scissor":
+                    temp = 0;
+                    break;
+            }
+            break;
+    }
+    switch (temp) {
+        case -2:
+            return "wtf";
+        case -1:
+            return "You Lose! " + computerSelection + " beats " + playerSelection;
+        case 0:
+            return "Draw!";
+        case 1:
+            return "You Win! " + playerSelection + " beats " + computerSelection;
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let selection = window.prompt("Select Rock Paper Scissor:");
+        console.log(rockPaperScissors(selection, computerPlay())+ "\n");
+    }
+}
